c6f64bf387018ff77502ecde959e0bcd6782d64f
Uichae <ghddmlco@gmail.com>
2023-09-05 15:20:55 +0900

    next study - update & delete 버튼 구현

---
+++ src/app/Control.js
@@ -0,0 +1,25 @@
+"use client";
+import { useParams } from "next/navigation";
+import Link from "next/link";
+
+export function Control() {
+  const params = useParams();
+  const id = params.id;
+  return (
+    <ul>
+      <li>
+        <Link href="/create">create</Link>
+      </li>
+      {id ? (
+        <>
+          <li>
+            <Link href={"/update/" + id}>update</Link>
+          </li>
+          <li>
+            <input type="button" value="delete" />
+          </li>
+        </>
+      ) : null}
+    </ul>
+  );
+}
